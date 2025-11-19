11eacdaddf835b1095879cef62700e940e7bb6f3
Daviasp <davia6660@gmail.com>
2025-11-18 22:03:04 -0300

    iniciando testes

---
+++ ProjetoPizzariav2/ProjetoPizzariav2/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjetoPizzariav2.Models
+{
+    public class Categoria
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public Categoria()
+        {
+
+        }
+
+    }
+}
