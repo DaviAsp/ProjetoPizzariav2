4ac366550d043530e513768bffcfc5f6456446e0
Daviasp <davia6660@gmail.com>
2025-12-22 21:32:32 -0300

    Fazendo avancos no reactstrap

--- ProjetoPizzariav2/ProjetoPizzariav2/clientapp/src/GerenciarAlunos.js
+++ ProjetoPizzariav2/ProjetoPizzariav2/clientapp/src/GerenciarAlunos.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { httpClient } from './HTTPClient';
+import { Input, Headings, Badge, Label } from "reactstrap";
 
 export default function GerenciarAlunos() {
 
@@ -37,13 +38,13 @@ export default function GerenciarAlunos() {
 
     let saida =
     <>
-        <h4>Gerenciar Aluno</h4>
-        <label>Nome</label><br/>
-        <input type="text" value={stateAluno.nomeAluno} onChange={(event) => { setStateAluno({ ...stateAluno, nomeAluno: event.target.value}) }}/><br/>
+        <h4>Gerenciar Alunos </h4>
+        <Label>Nome</Label><br/>
+        <Input  value={stateAluno.nomeAluno} onChange={(event) => { setStateAluno({ ...stateAluno, nomeAluno: event.target.value}) }}/><br/>
         <label>Email</label><br/>
-        <input type="email" value={stateAluno.email} onChange={(event) => { setStateAluno({ ...stateAluno, email: event.target.value}) }}/><br/>
+        <Input name="email"  type="email" value={stateAluno.email} onChange={(event) => { setStateAluno({ ...stateAluno, email: event.target.value}) }}/><br/>
         <label>Data de nascimento</label><br/>
-        <input type="date" value={stateAluno.dataNascimento} onChange={(event) => { setStateAluno({ ...stateAluno, dataNascimento: event.target.value}) }}/><br/><br/>
+        <Input type="date" value={stateAluno.dataNascimento} onChange={(event) => { setStateAluno({ ...stateAluno, dataNascimento: event.target.value}) }}/><br/><br/>
         <button onClick={gravarAluno}>Salvar</button>
     </>
 
--- ProjetoPizzariav2/ProjetoPizzariav2/clientapp/src/Menu.js
+++ ProjetoPizzariav2/ProjetoPizzariav2/clientapp/src/Menu.js
@@ -1,15 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {Link} from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 
-export default function Menu() {
+// export default function Menu() {
 
-    let saida = 
-    <nav className='menu'>
-        <Link to="/gerenciarAlunos">Gerenciar Alunos</Link> |
-        <Link to="/consultarAlunos">Consultar Alunos</Link>
-        <Link to="/matricular">Matricular</Link>
-    </nav> 
+//     let saida = 
+//     <nav className='menu'>
+//         <Link to="/gerenciarAlunos">Gerenciar Alunos</Link> |
+//         <Link to="/consultarAlunos">Consultar Alunos</Link>
+//         <Link to="/matricular">Matricular</Link>
+//     </nav> 
 
-    return saida;
-} 
+//     return saida;
+// } 
+
+
+
+const Menu = (props) => {
+
+const [isOpen, setIsOpen] = useState(false);
+
+const toggle = () => setIsOpen(!isOpen);
+
+  return (
+        <div>
+      <Navbar color="primary" dark expand="md">
+        <NavbarBrand href="/">Inicio</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/gerenciarAlunos">Alunos</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/consultarAlunos">
+                Consultar
+              </NavLink>
+            </NavItem>
+             <NavItem>
+              <NavLink href="/Matricular">
+                Matricular
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div> 
+  )
+
+}
+
+
+
+
+
+export default Menu;
+
+
+
+
+
+
+// export default function Menu(args) {
+//     const [isOpen, setIsOpen] = useState(false);
+
+//     const toggle = () => setIsOpen(!isOpen);
+//     let saida = 
+//     <div>
+//       <Navbar {...args}>
+//         <NavbarBrand href="/">reactstrap</NavbarBrand>
+//         <NavbarToggler onClick={toggle} />
+//         <Collapse isOpen={isOpen} navbar>
+//           <Nav className="me-auto" navbar>
+//             <NavItem>
+//               <NavLink href="/gerenciarAlunos">Alunos</NavLink>
+//             </NavItem>
+//             <NavItem>
+//               <NavLink href="/consultarAlunos">
+//                 Consultar
+//               </NavLink>
+//             </NavItem>
+//              <NavItem>
+//               <NavLink href="/Matricular">
+//                 Matricular
+//               </NavLink>
+//             </NavItem>
+//           </Nav>
+//         </Collapse>
+//       </Navbar>
+//     </div> 
+
+//     return saida;
+// } 
+
+
